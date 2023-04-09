2792d83dd3219092356831f477dcf5195f593a31
omron <omroners@gmail.com>
2023-04-09 17:58:21 +0300

    v8 Beta. DBConver add funtions

--- DBConverter.py
+++ DBConverter.py
@@ -1,4 +1,5 @@
 import openpyxl
+import time
 
 # Открываем файл Excel
 workbook = openpyxl.load_workbook('./NinzniyNovgorod/NizniyNovgorod.xlsx')
@@ -7,22 +8,92 @@ worksheet = workbook.active
 
 # Читаем значения ячеек в строке и преобразуем в массив
 
-Type_Baths = ['финская парная','русская баня','инфракрасная сауна','турецкая парная(хамам)']
+Type_Bath = {
+'финская парная' : 1,
+'русская баня': 2,
+'русская на дровах': 3,
+'турецкая парная(хамам)': 4,
+'инфракрасная сауна': 3,
+}
 
-for idx,cell in enumerate(worksheet['E']):
-    try:
-        # values = cell.value.split(',')
-        values = [x.strip() for x in cell.value.split(',')]
-        print(values)
-        # values = cell.value.replace(', ', ',')
-        print('Что в ячейке(массив): ', values)
-        print('Чистые значения: ',cell.value)
-        for words in values:
-            if words in Type_Baths:
-                print('Match')
-        idx += 1
-        print(idx)
-    except Exception as e:
-        print(e)
-        continue
+Type_Zones = {
+'бассейн' : 	1,
+'охраняемая парковка' : 	2,
+'холодная купель' : 	3,
+'обливное ведро' : 	4,
+'спутниковое тв' : 	5,
+'кондиционер' : 	6,
+'бильярд' : 	7,
+'кондиционер' : 	8,
+'бильярд' : 	9,
+'холодная купель' : 	10,
+'охраняемая парковка' : 	11,
+'прорубь' : 	12,
+'спутниковое тв' : 	13,
+'большой TV' : 	14,
+'обливное ведро' : 	15,
+'настольные игры' : 	16,
+'джакузи' : 	17,
+'Wi-Fi' : 	18,
+'караоке' : 	19,
+'японский офуро' : 	20,
+'загородный отдых' : 	21,
+'отель' : 	22,
+'бассейн спротивотоком' : 	23,
+'камин' : 	24
+}
+
+
+def bath_steam_room():
+    row = 1
+    for idx, cell in enumerate(worksheet['E'], start=1):
+        try:
+            values = [x.strip() for x in cell.value.split(',')]
+            print('Что в ячейке(массив): ', values)
+            print('Чистые значения: ', cell.value)
+            for words in values:
+                if words in Type_Bath:
+                    print('Совпало в словаре')
+                    worksheet.cell(row=row, column=16).value = Type_Bath[words]
+                    worksheet.cell(row=row, column=15).value = idx
+                    print('ID в словаре: ', Type_Bath[words])
+                    row += 1
+
+            idx += 1
+            print('Индекс: ', idx)
+        except Exception as e:
+            print(e)
+            continue
+
+        time.sleep(0.5)
+        workbook.save(filename="DBConvert.xlsx")
+
+
+def bath_zone():
+    row = 1
+    for idx, cell in enumerate(worksheet['H'], start=1):
+        try:
+            values = [x.strip() for x in cell.value.split(',')]
+            print('Что в ячейке(массив): ', values)
+            print('Чистые значения: ', cell.value)
+            for words in values:
+                if words in Type_Zones:
+                    print('Совпало в словаре')
+                    worksheet.cell(row=row, column=18).value = idx
+                    worksheet.cell(row=row, column=19).value = Type_Zones[words]
+                    print('ID в словаре: ', Type_Zones[words])
+                    row += 1
+
+            idx += 1
+            print('Индекс: ', idx)
+        except Exception as e:
+            print(e)
+            continue
+        time.sleep(0.5)
+        workbook.save(filename="DBImport/DBZones.xlsx")
+
+
+
+
+bath_zone()
 
